ead13349acbcaa7543b7922ee83c7cd1c31db5de
damir98dm <150920340+damir98dm@users.noreply.github.com>
2023-12-19 12:42:15 +0500

    Javascript_1

---
+++ app.js
@@ -0,0 +1,46 @@
+
+// Имя и возраст 
+
+var myName = prompt("Укажите свое имя");
+
+console.log('Меня зовут ' +  myName );
+
+var age = prompt('Укажите свой возраст');
+
+console.log('Мой возраст ' + age);
+
+// примеры
+
+var calc = prompt('Решите пример 6+3=');
+console.log('Пример 1: 6 + 3 = 9' + ' Ваш ответ : ' + calc);
+
+var calc = prompt('Решите пример 6-3=');
+console.log('Пример 2: 6 - 3 = 3' + ' Ваш ответ : ' + calc);
+
+var calc = prompt('Решите пример 6*3=');
+console.log('Пример 3: 6 * 3 = 18' + ' Ваш ответ : ' + calc);
+
+var calc = prompt('Решите пример 6/3=');
+console.log('Пример 4: 6 / 3 = 2' + ' Ваш ответ : ' + calc);
+
+var calc = prompt('Решите пример 40 % 20=');
+console.log('Пример 5: 40 % 20 = 8' + ' Ваш ответ : ' + calc);
+
+var cons = alert('Откройте консоль для просмотра');
+
+
+// среднее арифметическое
+
+var first = +prompt('Введите первое число' );
+console.log('Ваше первое число : ' + first );
+
+var sec = +prompt('Введите второе число' );
+console.log('Ваше второе число : ' + sec);
+
+var third = +prompt('Введите третье число' );
+console.log('Ваше третье число : ' + third);
+
+var res = +alert((first + sec + third ) / 3) ;
+
+
+
